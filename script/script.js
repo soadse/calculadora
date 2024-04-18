bf59c21e852e278666cc3752f76cbdd660f2dc85
Rodrigo Andrade <144704436+soadse@users.noreply.github.com>
2024-04-17 23:12:45 -0300

    ...

--- script/script.js
+++ script/script.js
@@ -1,17 +1,27 @@
 function addNum(value){
-    document.querySelector('#display').value += value 
+    var n1 = document.querySelector('#display').value += value 
+    if(String(n1).length > 8){
+        document.querySelector('#display').style.fontSize = '50px' 
+    }
 }
 
 function limpaNum(){
+    document.querySelector('#display').style.fontSize = '5em'
     document.querySelector('#display').value = ''
 }
 
 function somaNum(){
     try {
-        document.querySelector('#display').value = eval
+        var n2 = document.querySelector('#display').value = eval
         (document.querySelector('#display').value);
+        if(String(n2).length > 13){
+            document.querySelector('#display').style.fontSize = '35px'
+        }
     } catch (error) {
-        document.querySelector('#display').value = 'Erro';
+        document.querySelector('#display').value = 'Erro'; 
+        var erro = setTimeout(function errorr(){
+            document.querySelector('#display').value = ''
+        }, 500)
     }
 }
 
